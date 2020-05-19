80e1868d1fb55f2851d9c83b32bea85b5fa50736
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-05-19 17:43:26 -0400

    Fixed variable name.

--- linux_scripts/install_functions.sh
+++ linux_scripts/install_functions.sh
@@ -66,7 +66,7 @@ function create_basic_partitions() {
 
     if [[ "${windows_response}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
         # Creates one partition.  Partition uses the rest of the free space avalailable to create a Linux filesystem partition.
-        sgdisk -n 0:0:+${partition_size} -c "${partition_number2}":"Linux Filesystem" -t "${partition_number2}":8300 "${disk}"
+        sgdisk -n 0:0:+${partition_2_size} -c "${partition_number2}":"Linux Filesystem" -t "${partition_number2}":8300 "${disk}"
     else
         # Creates two partitions.  First one is a 512 MB EFI partition while the second uses the rest of the free space avalailable to create a Linux filesystem partition.
         sgdisk -n 0:0:+${partition_1_size} -c "${partition_number1}":"EFI System Partition" -t "${partition_number1}":ef00 "${disk}"
