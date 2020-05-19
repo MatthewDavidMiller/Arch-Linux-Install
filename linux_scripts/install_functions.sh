9ebb410d6ad6fb4c83417b67458782b533bd9135
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-05-19 14:46:33 -0400

    Fixed filename.

--- linux_scripts/install_functions.sh
+++ linux_scripts/install_functions.sh
@@ -154,7 +154,7 @@ function arch_install_base_packages_pacstrap() {
 }
 
 function arch_install_move_to_script_part_2() {
-    cp install_functions.sh '/mnt/install_functions'
+    cp install_functions.sh '/mnt/install_functions.sh'
     wget -O '/mnt/arch_linux_install_part_2.sh' 'https://raw.githubusercontent.com/MatthewDavidMiller/Arch-Linux-Install/stable/linux_scripts/arch_linux_install_part_2.sh'
     chmod +x '/mnt/arch_linux_install_part_2.sh'
     cat <<EOF >'/mnt/tmp/temp_variables.sh'
